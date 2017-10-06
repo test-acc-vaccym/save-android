f7b5a6a9e9b8bbba14cc65503e536bc55e48ced9
albalitz <albalitz@uni-bremen.de>
2017-10-06 21:15:15 +0200

    Fix bug making registration unusable
    Registering without having set up a user resulted in an exception when
    trying to authenticate which crashed the app.

--- Save/app/src/main/java/com/github/albalitz/save/persistence/api/Request.java
+++ Save/app/src/main/java/com/github/albalitz/save/persistence/api/Request.java
@@ -23,7 +23,9 @@ public class Request {
     }
 
     public static void post(String url, String username, String password, JSONObject json, AsyncHttpResponseHandler responseHandler) throws UnsupportedEncodingException {
-        client.setBasicAuth(username, password);
+        if (username != null && password != null) {
+            client.setBasicAuth(username, password);
+        }
 
         // See http://stackoverflow.com/questions/13052036/posting-json-xml-using-android-async-http-loopj
         // for the example I based this on.
